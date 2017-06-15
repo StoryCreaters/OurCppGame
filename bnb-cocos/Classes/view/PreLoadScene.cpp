d2039a149e29e71b79b6a8947b61e9e68798f424
Chris King <790132154@qq.com>
2017-06-15 15:42:24 +0800

    背景音乐的加载

--- bnb-cocos/Classes/view/PreLoadScene.cpp
+++ bnb-cocos/Classes/view/PreLoadScene.cpp
@@ -69,6 +69,9 @@ void PreLoadScene::loadSpriteFrame() {
 	for (auto s : gifts_animation) {
 		sprite_frame_cache->addSpriteFramesWithFile(s);
 	}
+	for (auto s : items_animation) {
+		sprite_frame_cache->addSpriteFramesWithFile(s);
+	}
 	for (auto s : person_animation) {
 		sprite_frame_cache->addSpriteFramesWithFile(s);
 	}
@@ -76,7 +79,12 @@ void PreLoadScene::loadSpriteFrame() {
 }
 
 void PreLoadScene::loadMusic() {
-	CocosDenshion::SimpleAudioEngine::getInstance()->preloadBackgroundMusic(to_load_music);
+	for (int i = 0; i < 3; i++) {
+		CocosDenshion::SimpleAudioEngine::getInstance()->preloadBackgroundMusic(to_load_music[i]);
+	}
+	for (int i = 0; i < 4; i++) {
+		CocosDenshion::SimpleAudioEngine::getInstance()->preloadEffect(load_effect[i]);
+	}
 }
 
 void PreLoadScene::progressUpdate() {
@@ -91,12 +99,10 @@ void PreLoadScene::progressUpdate() {
 		auto pft = ProgressFromTo::create(0.5f, _progress->getPercentage(), 100);
 		// 定义了一个CallFunc动作
 		auto callFunc = CallFunc::create([=] {
-			// 当加载资源完成，延迟2秒跳转到游戏主场景
-			auto delay = DelayTime::create(delaytime);
 			auto callFunc = CallFunc::create([] {
 				Director::getInstance()->replaceScene(OpenScene::createScene());
 			});
-			auto action = Sequence::create(delay, callFunc, NULL);
+			auto action = Sequence::create(callFunc, NULL);
 			this->runAction(action);
 		});
 		auto action = Sequence::create(pft, callFunc, NULL);
