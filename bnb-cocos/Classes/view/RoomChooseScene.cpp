7778aabdbad0d20e3b12649a3e83d93df993faaf
Chris King <790132154@qq.com>
2017-06-18 00:32:01 +0800

    钟大佬版本的RoomChoose

--- bnb-cocos/Classes/view/RoomChooseScene.cpp
+++ bnb-cocos/Classes/view/RoomChooseScene.cpp
@@ -26,31 +26,59 @@ bool RoomChoose::init() {
 	// 获得设备可见视图大小
 	vecs = { 0 };
 	Size visibleSize = Director::getInstance()->getVisibleSize();
-	auto backGround = cocos2d::Sprite::create("BackGround/GameOver.png");
+	auto backGround = cocos2d::Sprite::create("BackGround/chooseroom.png");
 	backGround->setPosition(cocos2d::Vec2(visibleSize.width / 2, visibleSize.height / 2));
 	this->addChild(backGround, 0);
 	cocos2d::Vec2 origin = cocos2d::Director::getInstance()->getVisibleOrigin();
 
-	//cocos2d::Vector<cocos2d::MenuItem*> vecs;
 
-	auto headline = Sprite::create("GameUI/RoomChoose.png");
-	headline->setPosition(cocos2d::Vec2(visibleSize.width / 2, visibleSize.height*0.75));
-	backGround->addChild(headline);
-	auto room_button = Button::create("GameUI/RoomCreate.png");
-	room_button->setPosition(Vec2(visibleSize.width / 2, visibleSize.height / 2));
-	room_button->addTouchEventListener([=](Ref* pSender, Widget::TouchEventType type) {
-		if (type == Widget::TouchEventType::ENDED) {
-			// 切换到Character Select场景
-			auto transition = TransitionShrinkGrow::create(2.0, Players::createScene());
-			Director::getInstance()->replaceScene(transition);
-			if (vecs.back() < 4) {
-				vecs.back()++;
-			}
-			else
-				addroom(this);
-		}
-	});
-	addChild(room_button);
+	cocos2d::Vector<cocos2d::MenuItem*> vecs;
+	std::string UiNames[] = { "GameUI/RoomChoose", "GameUI/Room1", "GameUI/Room2", "GameUI/Room3","GameUI/Room4" };
+	ccMenuCallback Uifuncs[] = { nullptr,CC_CALLBACK_1(RoomChoose::ToRoomOne, this), CC_CALLBACK_1(RoomChoose::ToRoomTwo, this),CC_CALLBACK_1(RoomChoose::ToRoomThree, this),CC_CALLBACK_1(RoomChoose::ToRoomFour, this) };
+	for (int i = 0; i < 5; ++i) {
+		auto menuI = cocos2d::MenuItemImage::create(UiNames[i] + ".png", UiNames[i] + "Selected.png", Uifuncs[i]);
+		if (i != 0)
+			menuI->setScale(0.8, 0.6);
+		vecs.pushBack(menuI);
+	}
+	LabelTTF* label_1;
+	label_1 = LabelTTF::create("Room Name:", "Arial", 32);
+	label_1->setColor(Color3B::BLACK);
+	label_1->setPosition(cocos2d::Vec2(visibleSize.width /2, visibleSize.height*0.73 ));
+	this->addChild(label_1);
+	//房间人数
+	auto * label = LabelTTF::create("Current people online :", "Arial", 32);
+	label->setColor(Color3B::BLACK);
+	label->setPosition(cocos2d::Vec2(visibleSize.width *0.8, visibleSize.height*0.73));
+	addChild(label);
+	for (int i = 0; i < 4; i++) {
+		LabelTTF* label;
+		//把这里的Number实现成现有人数
+		label = LabelTTF::create("Number:", "微软雅黑", 32);
+		label->setColor(Color3B::BLACK);
+		label->setPosition(cocos2d::Vec2(visibleSize.width *0.8, visibleSize.height*0.62 - i * 115));
+		this->addChild(label);
+	}
+
+	UImenus = cocos2d::Menu::createWithArray(vecs);
+	UImenus->setPosition(Vec2(visibleSize.width / 2, visibleSize.height / 2));
+	UImenus->alignItemsVertically();
+	addChild(UImenus);
+	//房间编号
+	auto * label_2 = LabelTTF::create("Room Number:", "Arial", 32);
+	label_2->setColor(Color3B::BLACK);
+	label_2->setPosition(cocos2d::Vec2(visibleSize.width *0.2, visibleSize.height*0.73));
+	addChild(label_2);
+	for (int i = 0; i < 4; i++) {
+		LabelTTF* label;
+		char str[5];
+		int j = 1000 + i;
+		itoa(j, str, 10);
+		label = LabelTTF::create(str, "微软雅黑", 32);
+		label->setColor(Color3B::BLACK);
+		label->setPosition(cocos2d::Vec2(visibleSize.width *0.2, visibleSize.height*0.62 - i * 115));
+		this->addChild(label);
+	}
 	// 创建“返回“按钮，点击时调用returnToMenu函数
 	auto return_button = Button::create("GameUI/b1.png");
 	return_button->setPosition(Vec2(visibleSize.width *0.85, visibleSize.height*0.18));
@@ -65,27 +93,28 @@ bool RoomChoose::init() {
 	return true;
 }
 
+
+//void RoomChoose::addroom(Ref* sender) {
+//	if (!vecs.empty()) {
+//		vecs.push_back(1);
+//		log("vecs[i]=%d", vecs.back());
+//	}
+//}
+//void RoomChoose::poproom( Ref* sender) {
+//	if (!vecs.empty()) {
+//		vecs.back()--;
+//	}
+//	
+//}
 void RoomChoose::ToRoomOne(Ref *sender) {
 	Director::getInstance()->replaceScene(Players::createScene());
 }
-void RoomChoose::addroom(Ref* sender) {
-	if (!vecs.empty()) {
-		vecs.push_back(1);
-		log("vecs[i]=%d", vecs.back());
-	}
+void RoomChoose::ToRoomTwo(Ref *sender) {
+	Director::getInstance()->replaceScene(Players::createScene());
 }
-void RoomChoose::poproom( Ref* sender) {
-	if (!vecs.empty()) {
-		vecs.back()--;
-	}
-	
+void RoomChoose::ToRoomThree(Ref *sender) {
+	Director::getInstance()->replaceScene(Players::createScene());
 }
-//void RoomChoose::ToRoomTwo(Ref *sender) {
-//	Director::getInstance()->replaceScene(Players::createScene());
-//}
-//void RoomChoose::ToRoomThree(Ref *sender) {
-//	Director::getInstance()->replaceScene(Players::createScene());
-//}
-//void RoomChoose::ToRoomFour(Ref *sender) {
-//	Director::getInstance()->replaceScene(Players::createScene());
-//}
+void RoomChoose::ToRoomFour(Ref *sender) {
+	Director::getInstance()->replaceScene(Players::createScene());
+}
